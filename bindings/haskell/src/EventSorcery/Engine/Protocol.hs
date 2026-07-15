4b87b2e83760e61d1d6f5421a195bd2f9d5bf28f
0xgleb <39841057+0xgleb@users.noreply.github.com>
2026-07-15 02:07:08 -0300

    feat: add deterministic Haskell engine codecs

---
+++ bindings/haskell/src/EventSorcery/Engine/Protocol.hs
@@ -0,0 +1,64 @@
+module EventSorcery.Engine.Protocol (
+  AggregateId (..),
+  AggregateType (..),
+  EventType (..),
+  EventVersion (..),
+  OpenOptions (..),
+  ProposedEvent (..),
+  StoredEvent (..),
+  StreamIdentity (..),
+) where
+
+import Data.ByteString (ByteString)
+import Data.Text (Text)
+import Data.Word (Word32, Word64)
+import Prelude (Eq, Show)
+
+
+newtype AggregateType = AggregateType Text
+  deriving newtype (Eq, Show)
+
+
+newtype AggregateId = AggregateId Text
+  deriving newtype (Eq, Show)
+
+
+newtype EventType = EventType Text
+  deriving newtype (Eq, Show)
+
+
+newtype EventVersion = EventVersion Text
+  deriving newtype (Eq, Show)
+
+
+data OpenOptions = OpenOptions
+  { path :: Text
+  , busyTimeoutMilliseconds :: Word64
+  , poolSize :: Word32
+  , runtimeThreads :: Word32
+  }
+  deriving stock (Eq, Show)
+
+
+data StreamIdentity = StreamIdentity
+  { aggregateType :: AggregateType
+  , aggregateId :: AggregateId
+  }
+  deriving stock (Eq, Show)
+
+
+data ProposedEvent = ProposedEvent
+  { eventType :: EventType
+  , eventVersion :: EventVersion
+  , payload :: ByteString
+  }
+  deriving stock (Eq, Show)
+
+
+data StoredEvent = StoredEvent
+  { sequence :: Word64
+  , eventType :: EventType
+  , eventVersion :: EventVersion
+  , payload :: ByteString
+  }
+  deriving stock (Eq, Show)
