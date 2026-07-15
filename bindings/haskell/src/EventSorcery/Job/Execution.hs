b7e7f1e54451cdf08fce3b1850fbce2bde6342c8
0xgleb <39841057+0xgleb@users.noreply.github.com>
2026-07-15 13:44:09 -0300

    feat: route Haskell job executions

---
+++ bindings/haskell/src/EventSorcery/Job/Execution.hs
@@ -0,0 +1,90 @@
+module EventSorcery.Job.Execution (
+  DurableJob (..),
+  JobAttempt (..),
+  JobContext (..),
+  JobFailure (..),
+  JobOutcome (..),
+  Reconciliation (..),
+  executeDurableJob,
+) where
+
+import Data.Kind (Type)
+import Data.Text (Text)
+import Data.Word (Word32)
+import EventSorcery.Job (
+  Job (JobError, JobOutput),
+  JobExecutionRoute (ReconcileExecution, SubmitExecution),
+  JobId,
+  JobInstant,
+ )
+import Prelude (Either (Left, Right), Eq, IO, Show, pure)
+
+
+newtype JobAttempt = JobAttempt Word32
+  deriving stock (Eq, Show)
+
+
+data JobContext = JobContext
+  { jobId :: JobId
+  , attempt :: JobAttempt
+  }
+  deriving stock (Eq, Show)
+
+
+data JobOutcome output
+  = JobDone output
+  | JobDeferred JobInstant
+  deriving stock (Eq, Show)
+
+
+data JobFailure failure
+  = TransientFailure failure
+  | TerminalFailure failure
+  deriving stock (Eq, Show)
+
+
+data Reconciliation output
+  = Reconciled output
+  | NotSubmitted
+  | Indeterminate JobInstant
+  deriving stock (Eq, Show)
+
+
+class Job job => DurableJob job where
+  type JobInput job :: Type
+
+
+  renderJobError :: job -> JobError job -> Text
+
+
+  submit
+    :: JobContext
+    -> JobInput job
+    -> job
+    -> IO (Either (JobFailure (JobError job)) (JobOutcome (JobOutput job)))
+
+
+  reconcile
+    :: JobContext
+    -> JobInput job
+    -> job
+    -> IO (Either (JobFailure (JobError job)) (Reconciliation (JobOutput job)))
+
+
+executeDurableJob
+  :: DurableJob job
+  => JobContext
+  -> JobExecutionRoute
+  -> JobInput job
+  -> job
+  -> IO (Either (JobFailure (JobError job)) (JobOutcome (JobOutput job)))
+executeDurableJob context route input job = case route of
+  SubmitExecution -> submit context input job
+  ReconcileExecution -> do
+    result <- reconcile context input job
+
+    case result of
+      Left failure -> pure (Left failure)
+      Right (Reconciled output) -> pure (Right (JobDone output))
+      Right NotSubmitted -> submit context input job
+      Right (Indeterminate runAt) -> pure (Right (JobDeferred runAt))
