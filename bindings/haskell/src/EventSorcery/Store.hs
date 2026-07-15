bd1e5617a1af883face0e9960766fb4efe986fb2
0xgleb <39841057+0xgleb@users.noreply.github.com>
2026-07-15 07:35:35 -0300

    feat: execute typed Haskell commands

---
+++ bindings/haskell/src/EventSorcery/Store.hs
@@ -0,0 +1,245 @@
+module EventSorcery.Store (
+  Store,
+  StoreError (..),
+  executeCommand,
+  loadEntity,
+  mkStore,
+) where
+
+import Control.Monad (foldM)
+import Data.Bifunctor (first)
+import Data.Foldable (foldl')
+import Data.List.NonEmpty (NonEmpty ((:|)))
+import Data.List.NonEmpty qualified as NonEmpty
+import Data.Unrestricted.Linear (Ur (Ur))
+import Data.Word (Word64)
+import EventSorcery.Aggregate (Effect (..), EventSourced, EventVersion (..))
+import EventSorcery.Aggregate qualified as Aggregate
+import EventSorcery.Engine (EngineError)
+import EventSorcery.Engine qualified as Engine
+import EventSorcery.Job (
+  Job (encodeJob),
+  JobId,
+  JobInstant (JobInstant),
+  JobKind (JobKind),
+  JobSeed (JobSeed),
+  commitWithJob,
+  jobType,
+ )
+import EventSorcery.Stream (
+  ProposedEvent (ProposedEvent),
+  ReplayError,
+  StoredEvent (StoredEvent),
+  StreamIdentity,
+  StreamKey,
+  commit,
+  loadStream,
+  replay,
+  streamKeyIdentity,
+ )
+import Prelude (
+  Either (..),
+  Eq,
+  IO,
+  Maybe (..),
+  Show,
+  fmap,
+  fst,
+  pure,
+  (<$),
+ )
+
+
+data Store entity = Store Engine.Store (IO JobId)
+
+
+data StoreError entity
+  = StoreEngineFailed EngineError
+  | StoreReplayFailed (ReplayError entity)
+  | StoreCommandRejected (Aggregate.CommandError entity)
+  | StoreDecisionRejected (Aggregate.ApplyError entity)
+
+
+deriving stock instance
+  ( Eq (Aggregate.ApplyError entity)
+  , Eq (Aggregate.CommandError entity)
+  )
+  => Eq (StoreError entity)
+
+
+deriving stock instance
+  ( Show (Aggregate.ApplyError entity)
+  , Show (Aggregate.CommandError entity)
+  )
+  => Show (StoreError entity)
+
+
+data PreparedCommand entity where
+  PreparedCommand
+    :: Ur (CommitPlan entity)
+    %1 -> PreparedCommand entity
+
+
+data CommitPlan entity
+  = PreparedEvents entity Word64 (NonEmpty ProposedEvent)
+  | PreparedDispatch entity Word64 (NonEmpty ProposedEvent) JobSeed
+
+
+mkStore :: Engine.Store -> IO JobId -> Store entity
+mkStore = Store
+
+
+loadEntity
+  :: forall entity
+   . EventSourced entity
+  => Store entity
+  -> StreamKey entity
+  -> IO (Either (StoreError entity) (Maybe entity))
+loadEntity store key = fmap (fmap fst) (loadCurrent store key)
+
+
+executeCommand
+  :: forall entity
+   . EventSourced entity
+  => Store entity
+  -> StreamKey entity
+  -> Aggregate.Command entity
+  -> IO (Either (StoreError entity) entity)
+executeCommand store@(Store engine _) key command = do
+  loaded <- loadCurrent store key
+
+  case loaded of
+    Left failure -> pure (Left failure)
+    Right (current, expected) ->
+      case decide current of
+        Left failure -> pure (Left (StoreCommandRejected failure))
+        Right effect -> do
+          prepared <- prepareEffect store expected current effect
+
+          case prepared of
+            Left failure -> pure (Left failure)
+            Right preparedCommand ->
+              commitPrepared
+                engine
+                (streamKeyIdentity key)
+                preparedCommand
+  where
+    decide Nothing = Aggregate.initialize command
+    decide (Just entity) = Aggregate.transition entity command
+
+
+loadCurrent
+  :: EventSourced entity
+  => Store entity
+  -> StreamKey entity
+  -> IO (Either (StoreError entity) (Maybe entity, Word64))
+loadCurrent (Store engine _) key = do
+  loaded <- loadStream engine (streamKeyIdentity key) Nothing
+
+  pure do
+    events <- first StoreEngineFailed loaded
+    entity <- first StoreReplayFailed (replay key events)
+
+    pure (entity, latestSequence events)
+
+
+prepareEffect
+  :: EventSourced entity
+  => Store entity
+  -> Word64
+  -> Maybe entity
+  -> Effect entity
+  -> IO (Either (StoreError entity) (PreparedCommand entity))
+prepareEffect _ expected current (Events events) =
+  pure do
+    next <- first StoreDecisionRejected (applyEvents current events)
+
+    pure
+      ( PreparedCommand
+          (Ur (PreparedEvents next expected (encodeEvents events)))
+      )
+prepareEffect (Store _ nextJobId) expected current (Dispatch job) = do
+  identifier <- nextJobId
+  let intent = Aggregate.injectDispatchIntent (Aggregate.dispatchIntent identifier job)
+
+  pure do
+    next <-
+      first
+        StoreDecisionRejected
+        (applyEvents current (intent :| []))
+    let seed =
+          JobSeed
+            identifier
+            (jobKind job)
+            (encodeJob job)
+            (JobInstant 0)
+
+    pure
+      ( PreparedCommand
+          ( Ur
+              ( PreparedDispatch
+                  next
+                  expected
+                  (encodeEvents (intent :| []))
+                  seed
+              )
+          )
+      )
+
+
+commitPrepared
+  :: Engine.Store
+  -> StreamIdentity
+  -> PreparedCommand entity
+  %1 -> IO (Either (StoreError entity) entity)
+commitPrepared engine identity (PreparedCommand (Ur plan)) =
+  case plan of
+    PreparedEvents next expected events -> do
+      committed <- commit engine identity expected events
+
+      pure (next <$ first StoreEngineFailed committed)
+    PreparedDispatch next expected events seed -> do
+      committed <- commitWithJob engine identity expected events seed
+
+      pure (next <$ first StoreEngineFailed committed)
+
+
+applyEvents
+  :: EventSourced entity
+  => Maybe entity
+  -> NonEmpty (Aggregate.Event entity)
+  -> Either (Aggregate.ApplyError entity) entity
+applyEvents Nothing (firstEvent :| remaining) = do
+  initial <- Aggregate.originate firstEvent
+  foldM Aggregate.evolve initial remaining
+applyEvents (Just entity) events =
+  foldM Aggregate.evolve entity events
+
+
+encodeEvents
+  :: EventSourced entity
+  => NonEmpty (Aggregate.Event entity)
+  -> NonEmpty ProposedEvent
+encodeEvents = NonEmpty.map encodeEvent
+
+
+encodeEvent
+  :: EventSourced entity
+  => Aggregate.Event entity
+  -> ProposedEvent
+encodeEvent event =
+  let EventVersion version = Aggregate.eventVersion event
+   in ProposedEvent
+        (Aggregate.eventType event)
+        version
+        (Aggregate.encodeEvent event)
+
+
+jobKind :: forall job. Job job => job -> JobKind
+jobKind _ = JobKind (jobType @job)
+
+
+latestSequence :: [StoredEvent] -> Word64
+latestSequence = foldl' useSequence 0
+  where
+    useSequence _ (StoredEvent sequence _ _ _) = sequence
